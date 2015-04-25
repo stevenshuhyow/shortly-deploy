b15cb2b3a29293511229dc3e8b0518e865cead03
stevenshuhyow <stevenshuhyow@gmail.com>
2015-04-24 19:37:33 -0700

    Implemented concat and IP address

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,13 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['node_modules/*.js']
+      }
+
     },
 
     mochaTest: {
